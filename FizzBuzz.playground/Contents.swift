b2fd4882682d33f7450a9e89040423d932d88b31
idevF <124703341+idevF@users.noreply.github.com>
2023-02-28 16:16:09 +0300

    Solution of FizzBuzz question with if else

--- FizzBuzz.playground/Contents.swift
+++ FizzBuzz.playground/Contents.swift
@@ -1,4 +1,4 @@
-import UIKit
+import Cocoa
 
 /* Write a program that iterates over numbers 1 to 100. Print out a number for every number except multiples of three, in which case output Fizz. For multiples of five, output Buzz, and for multiples of 3 and 5 output FizzBuzz. */
 
